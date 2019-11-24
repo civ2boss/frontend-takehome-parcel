672ae82aca497e142b4324e6989763f720f96640
Robin Ting <civ2boss@gmail.com>
2019-11-24 13:17:55 -0500

    Refactor favorite button into separate file

---
+++ src/components/favorite-button.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function FavoriteButton({ result, favorites, setFavorites }) {
+  const handleFavorite = () => {
+    const foundIndex = favorites.findIndex(entry => entry.sha === result.sha);
+    if (foundIndex >= 0) {
+      const newFavorites = [...favorites];
+      newFavorites.splice(foundIndex, 1);
+      setFavorites(newFavorites);
+    } else {
+      setFavorites([...favorites, result]);
+    }
+  };
+
+  return (
+    <button type="button" className="favorite" onClick={handleFavorite}>
+      {favorites.find(entry => entry === result) ? (
+        <i className="fas fa-heart" />
+      ) : (
+        <i className="fal fa-heart" />
+      )}
+    </button>
+  );
+}
+
+FavoriteButton.propTypes = {
+  result: PropTypes.object,
+  favorites: PropTypes.array,
+  setFavorites: PropTypes.func,
+};
+
+export default FavoriteButton;
--- src/components/results.js
+++ src/components/results.js
@@ -1,28 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-function FavoriteButton({ result, favorites, setFavorites }) {
-  const handleFavorite = () => {
-    const foundIndex = favorites.findIndex(entry => entry.sha === result.sha);
-    if (foundIndex >= 0) {
-      const newFavorites = [...favorites];
-      newFavorites.splice(foundIndex, 1);
-      setFavorites(newFavorites);
-    } else {
-      setFavorites([...favorites, result]);
-    }
-  };
-
-  return (
-    <button type="button" className="favorite" onClick={handleFavorite}>
-      {favorites.find(entry => entry === result) ? (
-        <i className="fas fa-heart" />
-      ) : (
-        <i className="fal fa-heart" />
-      )}
-    </button>
-  );
-}
+import FavoriteButton from './favorite-button';
 
 function Results({ results, favorites, setFavorites }) {
   return (
@@ -68,12 +47,6 @@ function Results({ results, favorites, setFavorites }) {
 
 Results.propTypes = {
   results: PropTypes.array,
-  favorites: PropTypes.array,
-  setFavorites: PropTypes.func,
-};
-
-FavoriteButton.propTypes = {
-  result: PropTypes.object,
   favorites: PropTypes.array,
   setFavorites: PropTypes.func,
 };
